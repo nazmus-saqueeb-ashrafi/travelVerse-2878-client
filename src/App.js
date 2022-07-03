408d9dd160f09371b8c25d6c51546bab56d31860
Nazmus Ashrafi <nazmus.s.ashrafi@gmail.com>
2022-07-03 19:39:36 +0600

    added Profile and Login pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css"
+import Login from "./pages/auth/Login";
 import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile"
+
 function App() {
   return (
     <div className="App">
-        <Home/>
+        {/* <Home/> */}
+        <Profile/>
+        {/* <Login/> */}
     </div>
   );
 }
